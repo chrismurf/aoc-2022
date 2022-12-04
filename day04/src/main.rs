727412dea48ee0bdda7aa46537408621b8564861
Chris Murphy <chrismurf@gmail.com>
2022-12-04 01:03:44 -0500

    Minor cleanup.

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -2,23 +2,14 @@ use std::fs::File;
 use std::collections::HashSet;
 use std::io::{self, prelude::*, BufReader};
 
-fn fully_overlapping(area_a : &HashSet<u32>, area_b : &HashSet<u32>) -> bool {
-    return area_a.is_subset(area_b) || area_b.is_subset(area_a);
-}
-
-fn overlapped_at_all(area_a : &HashSet<u32>, area_b : &HashSet<u32>) -> bool {
-    return area_a.intersection(area_b).count() > 0;
-}
-
 fn day04() {
-    let file = File::open("input.txt")
-        .unwrap_or_else(|_| panic!("File 'input.txt' not readable.") );
+    let file = File::open("input.txt").expect("File 'input.txt' not readable.");
     let reader = BufReader::new(file)
         .lines() // Get a line iterator
         .filter_map(|line| line.ok()); // Get Strings instead of Result
 
-    let mut count: u32 = 0;
-    let mut count2: u32 = 0;
+    let mut fully_overlapping: u32 = 0;
+    let mut partially_overlapping: u32 = 0;
 
     for line in reader {
         let areas: Vec<HashSet<u32>> = line
@@ -30,16 +21,17 @@ fn day04() {
             ).map(|a| (a[0]..=a[1]).collect()       // Create 2x HashSet<u32> with [min..=max]
         ).collect();                                // Collect assignments into a vector
 
-        if fully_overlapping(&areas[0], &areas[1]) { 
-            count += 1;
+        if areas[0].is_subset(&areas[1]) || areas[1].is_subset(&areas[0]) {
+            fully_overlapping += 1;
         }
-        if overlapped_at_all(&areas[0], &areas[1]) { 
-            count2 += 1;
+
+        if areas[0].intersection(&areas[1]).count() > 0 {
+            partially_overlapping += 1;
         }
     }
 
-    println!("Final score for Part 1: {}", count);
-    println!("Final score for Part 2: {}", count2);
+    println!("Part 1: {} are fully overlapping.", fully_overlapping);
+    println!("Part 2: {} are partially overlapping.", partially_overlapping);
 }
 
 fn main() -> io::Result<()> {
